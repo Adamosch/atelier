90a25685862325c30186d934e5e9355578b88af8
Adam <adam.majkowski93@gmail.com>
2017-11-15 19:07:10 +0100

    Add mailers for reservation reminder

---
+++ app/mailers/book_notifier_mailer.rb
@@ -0,0 +1,18 @@
+class BookNotifierMailer < ApplicationMailer
+
+  def book_return_remind(book)
+    @book = book
+    @reservation = book.reservations.find_by(status: "TAKEN")
+    @borrower = @reservation.user
+
+    mail(to: @borrower.email, subject: "Upływa termin zwrotu książki #{@book.title}")
+  end
+
+  def book_reserved_return(book)
+    @book = book
+    @reservation = book.reservations.find_by(status: "TAKEN")
+    @borrower = @reservation.user
+
+    mail(to: @borrower.email, subject: "Książka #{@book.title} będzie niedługo zwrócona!")
+  end
+end
