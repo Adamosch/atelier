8d31780927a60dbee0f4dac322e66f608122bd8f
Adam <adam.majkowski93@gmail.com>
2017-11-22 21:36:39 +0100

    Remove commented method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,4 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :reservations
-
-  # def self.from_omniauth(access_token)
-  #   data = access_token.info
-  #   user = User.where(email: data['email']).first
-  #
-  #   unless user
-  #     user = User.create(
-  #         email: data['email'],
-  #         password: Devise.friendly_token[0, 20]
-  #     )
-  #   end
-  #   user
-  # end
 end
